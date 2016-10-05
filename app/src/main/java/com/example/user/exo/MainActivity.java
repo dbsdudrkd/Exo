97973bfd4020217bc4595a9e4d3b07568b711834
USER <aa@aa.com>
2016-10-05 12:01:49 +0900

    ii

--- app/src/main/java/com/example/user/exo/MainActivity.java
+++ app/src/main/java/com/example/user/exo/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         final String[] mid = {"히어로즈", "24시", "로스트", "로스트룸", "스몰빌", "탐정몽크", "빅뱅이론", "프렌즈", "덱스터", "글리", "가쉽걸", "테이큰", "슈퍼내추럴", "브이"};
 
         // List
+        // abc
     }
 }
