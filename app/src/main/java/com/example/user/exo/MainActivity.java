916a25c7cd958ee3af891b3533fcac23204e6021
USER <aaa@aaa.com>
2016-10-05 11:48:19 +0900

    m

--- app/src/main/java/com/example/user/exo/MainActivity.java
+++ app/src/main/java/com/example/user/exo/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("리스트뷰 테스트");
+
+        final String[] mid = {"히어로즈", "24시", "로스트", "로스트룸", "스몰빌", "탐정몽크", "빅뱅이론", "프렌즈", "덱스터", "글리", "가쉽걸", "테이큰", "슈퍼내추럴", "브이"};
+
+        // List
     }
 }
